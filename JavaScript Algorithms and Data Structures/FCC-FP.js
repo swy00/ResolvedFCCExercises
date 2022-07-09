b8673f6e15bcdcc43e0d7d42d1ca086aed2ad35a
swy00 <franciscoronga00@gmail.com>
2022-07-09 06:07:36 -0300

    Functional Prog

---
+++ JavaScript Algorithms and Data Structures/FCC-FP.js
@@ -0,0 +1,37 @@
+/// Functional Programming
+
+
+//Learn About Functional Programming
+
+/*Functional programming is a style of programming where solutions are simple, isolated functions, without any side effects outside of the function scope: 
+INPUT -> PROCESS -> OUTPUT
+
+Functional programming is about:
+
+Isolated functions - there is no dependence on the state of the program, which includes global variables that are subject to change
+
+Pure functions - the same input always gives the same output
+
+Functions with limited side effects - any changes, or mutations, to the state of the program outside the function are carefully controlled */
+
+const prepareTea = () => 'greenTea'; // Function that returns a string representing a cup of green tea
+
+/*Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+const tea4TeamFCC = getTea(40);
+
+//Understand Functional Programming Terminology
+
+
+
