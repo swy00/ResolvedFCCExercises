44b27f52809129d13c277d13043e06942d6f3996
swy00 <franciscoronga00@gmail.com>
2022-08-13 16:51:22 -0300

    new

--- JavaScript Algorithms and Data Structures/FCC-FP.js
+++ JavaScript Algorithms and Data Structures/FCC-FP.js
@@ -20,7 +20,7 @@ const prepareTea = () => 'greenTea'; // Function that returns a string represent
 following function returns an array of strings (each representing a cup of
 a specific type of tea).
 */
-const getTea = (numOfCups) => {
+const getTeaa = (numOfCups) => {
   const teaCups = [];
 
   for(let cups = 1; cups <= numOfCups; cups += 1) {
@@ -29,9 +29,114 @@ const getTea = (numOfCups) => {
   }
   return teaCups;
 };
-const tea4TeamFCC = getTea(40);
+const tea4TeamFCC = getTeaa(40);
 
 //Understand Functional Programming Terminology
 
+/*Callbacks are the functions that are slipped or passed into another function to decide the invocation of that function. You may have seen them passed to other methods, for example in filter, the callback function tells JavaScript the criteria for how to filter an array.
 
+Functions that can be assigned to a variable, passed into another function, or returned from another function just like any other normal value, are called first class functions. In JavaScript, all functions are first class functions.
+
+The functions that take a function as an argument, or return a function as a return value are called higher order functions.
+
+When functions are passed in to or returned from another function, then those functions which were passed in or returned can be called a lambda. */
+
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+const tea4GreenTeamFCC = getTea(prepareGreenTea,27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea,13);
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
+
+/*Understand the Hazards of Using Imperative Code
+
+Functional programming is a good habit. It keeps your code easy to manage, and saves you from sneaky bugs. 
+But before we get there, let's look at an imperative approach to programming to highlight where you may have issues.
+
+In English (and many other languages), the imperative tense is used to give commands. 
+Similarly, an imperative style in programming is one that gives the computer a set of statements to perform a task.
+
+Often the statements change the state of the program, like updating global variables. 
+A classic example is writing a for loop that gives exact directions to iterate over the indices of an array.
+
+In contrast, functional programming is a form of declarative programming. You tell the computer what you want done by calling a method or function.
+
+JavaScript offers many predefined methods that handle common tasks so you don't need to write out how the computer should perform them. 
+For example, instead of using the for loop mentioned above, you could call the map method which handles the details of iterating over an array. 
+This helps to avoid semantic errors, like the "Off By One Errors" that were covered in the Debugging section.
+
+Consider the scenario: you are browsing the web in your browser, and want to track the tabs you have opened. Let's try to model this using some simple object-oriented code.
+
+A Window object is made up of tabs, and you usually have more than one Window open. The titles of each open site in each Window object is held in an array. 
+After working in the browser (opening new tabs, merging windows, and closing tabs), you want to print the tabs that are still open. 
+Closed tabs are removed from the array and new tabs (for simplicity) get added to the end of it.
+
+The code editor shows an implementation of this functionality with functions for tabOpen(), tabClose(), and join(). 
+The array tabs is part of the Window object that stores the name of the open pages. */
+
+// tabs is an array of titles of each site open within the window
+const Window = function(tabs) {
+  this.tabs = tabs; // We keep a record of the array inside the object
+};
+
+// When you join two windows into one window
+Window.prototype.join = function(otherWindow) {
+  this.tabs = this.tabs.concat(otherWindow.tabs);
+  return this;
+};
+
+// When you open a new tab at the end
+Window.prototype.tabOpen = function(tab) {
+  this.tabs.push('new tab'); // Let's open a new tab for now
+  return this;
+};
+
+// When you close a tab
+Window.prototype.tabClose = function(index) {
+
+  const tabsBeforeIndex = this.tabs.splice(0, index); // Get the tabs before the tab
+  const tabsAfterIndex = this.tabs.splice(1); // Get the tabs after the tab
+
+/*Despues de que la segunda linea se ejecuta en la array actual[‘Vimeo’, ‘Vine’], 
+siempre va a omitir el primer value (index 0) y el que tiene index 1 hasta el final, llegando a lo que se quiere. */
+
+  this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // Join them together
+
+
+  return this;
+ };
+
+// Let's create three browser windows
+const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
+
+// Now perform the tab opening, closing, and other operations
+const finalTabs = socialWindow
+  .tabOpen() // Open a new tab for cat memes
+  .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
+  .join(workWindow.tabClose(1).tabOpen());
+console.log(finalTabs.tabs);
 
