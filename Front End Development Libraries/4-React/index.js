c3c5c7e4e939053a02935528a1d1e936c459d3a8
francisco ronga <franciscoronga00@gmail.com>
2022-09-15 22:26:43 -0300

    new

--- Front End Development Libraries/4-React/index.js
+++ Front End Development Libraries/4-React/index.js
@@ -95,7 +95,6 @@ const JSX1 = (
     <p>Lets render this to the DOM</p>
   </div>
 );
-// Change code below this line
 ReactDOM.render(JSX1,document.getElementById("challenge-node"))
 
 /*
@@ -250,10 +249,7 @@ class ParentComponent extends React.Component {
     return (
       <div>
         <h1>I am the parent</h1>
-        { /* Change code below this line */ }
         <ChildComponent />
-
-        { /* Change code above this line */ }
       </div>
     );
   }
@@ -291,9 +287,7 @@ const TypesOfFruit = () => {
 const Fruits = () => {
   return (
     <div>
-      { /* Change code below this line */ }
-      <TypesOfFruit/>
-      { /* Change code above this line */ }
+      <TypesOfFruit/>     
     </div>
   );
 };
@@ -306,10 +300,8 @@ class TypesOfFood extends React.Component {
   render() {
     return (
       <div>
-        <h1>Types of Food:</h1>
-        { /* Change code below this line */ }
-        <Fruits/>
-        { /* Change code above this line */ }
+        <h1>Types of Food:</h1>      
+        <Fruits/>       
       </div>
     );
   }
@@ -327,7 +319,121 @@ Nest two components inside of Fruits — first NonCitrus, and then Citrus. Both 
 Next, nest the Fruits class component into the TypesOfFood component, below the h1 heading element and above Vegetables. 
 The result should be a series of nested components, which uses two different component types.*/
 
+class Fruits2 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Fruits:</h2>
+          <NonCitrus />
+          <Citrus/>
+      </div>
+    );
+  }
+};
 
+class TypesOfFood extends React.Component {
+  constructor(props) {
+     super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+          <Fruits2/>
+        <Vegetables />
+      </div>
+    );
+  }
+};
+
+/*
+Render a Class Component to the DOM
+You may remember using the ReactDOM API in an earlier challenge to render JSX elements to the DOM. The process for rendering React components will look very similar. 
+The past few challenges focused on components and composition, so the rendering was done for you behind the scenes. 
+However, none of the React code you write will render to the DOM without making a call to the ReactDOM API.
+
+Here's a refresher on the syntax: ReactDOM.render(componentToRender, targetNode). The first argument is the React component that you want to render. 
+The second argument is the DOM node that you want to render that component within.
+
+React components are passed into ReactDOM.render() a little differently than JSX elements. 
+For JSX elements, you pass in the name of the element that you want to render. However, for React components, you need to use the 
+same syntax as if you were rendering a nested component, for example ReactDOM.render(<ComponentToRender />, targetNode). 
+You use this syntax for both ES6 class components and functional components.
+
+Both the Fruits and Vegetables components are defined for you behind the scenes. 
+Render both components as children of the TypesOfFood component, then render TypesOfFood to the DOM. There is a div with id='challenge-node' available for you to use. */
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* Change code below this line */}
+        <Fruits/>
+        <Vegetables/>
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+};
+// Change code below this line
+ReactDOM.render(<TypesOfFood />,document.getElementById("challenge-node"))
+
+/*
+Write a React Component from Scratch
+Now that you've learned the basics of JSX and React components, it's time to write a component on your own. 
+React components are the core building blocks of React applications so it's important to become very familiar with writing them. 
+Remember, a typical React component is an ES6 class which extends React.Component. It has a render method that returns HTML (from JSX) or null. 
+This is the basic form of a React component. Once you understand this well, you will be prepared to start building more complex React projects.
+
+Define a class MyComponent that extends React.Component. 
+Its render method should return a div that contains an h1 tag with the text: My First React Component! in it. 
+Use this text exactly, the case and punctuation matter. Make sure to call the constructor for your component, too.
+
+Render this component to the DOM using ReactDOM.render(). There is a div with id='challenge-node' available for you to use. */
+
+// Change code below this line
+
+class MyComponent extends React.Component{
+  constructor (props){
+    super(props);
+  }
+  render(){
+    return (
+      <div>
+      <h1>My First React Component!</h1>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<MyComponent/>, document.getElementById("challenge-node"))
+
+/*
+Pass Props to a Stateless Functional Component
+The previous challenges covered a lot about creating and composing JSX elements, functional components, and ES6 style class components in React. 
+With this foundation, it's time to look at another feature very common in React: props. In React, you can pass props, or properties, to child components. 
+Say you have an App component which renders a child component called Welcome which is a stateless functional component. You can pass Welcome a user property by writing:
+
+<App>
+  <Welcome user='Mark' />
+</App>
+You use custom HTML attributes created by you and supported by React to be passed to the component. 
+In this case, the created property user is passed to the component Welcome. Since Welcome is a stateless functional component, it has access to this value like so:
+
+const Welcome = (props) => <h1>Hello, {props.user}!</h1>
+It is standard to call this value props and when dealing with stateless functional components, you basically consider it as an argument to a function which returns JSX. 
+You can access the value of the argument in the function body. With class components, you will see this is a little different.
+
+There are Calendar and CurrentDate components in the code editor. 
+When rendering CurrentDate from the Calendar component, pass in a property of date assigned to the current date from JavaScript's Date object. 
+Then access this prop in the CurrentDate component, showing its value within the p tags. 
+Note that for prop values to be evaluated as JavaScript, they must be enclosed in curly brackets, for instance date={Date()}. */
 
 
 
