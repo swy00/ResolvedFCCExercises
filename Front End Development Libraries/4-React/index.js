135b85e86e6277055e6a73b5f2711d7f2c0d79f1
Swy00 <franciscoronga00@gmail.com>
2022-11-04 18:11:55 -0300

    new

--- Front End Development Libraries/4-React/index.js
+++ Front End Development Libraries/4-React/index.js
@@ -1194,7 +1194,96 @@ Next, add RenderInput to the render method in MyApp, then create a prop called i
  Observe how the data flows between the components and how the single source of truth remains the state of the parent component. 
  Admittedly, this example is a bit contrived, but should serve to illustrate how data and callbacks can be passed between React components. */
 
- 
+ class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
+  render() {
+    return (
+       <div>
+        { /* Change code below this line */ }
+        <GetInput input={this.state.inputValue} handleChange={this.handleChange} />
+        <RenderInput input={this.state.inputValue}  />
+        { /* Change code above this line */ }
+       </div>
+    );
+  }
+};
+
+class GetInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Get Input:</h3>
+        <input
+          value={this.props.input}
+          onChange={this.props.handleChange}/>
+      </div>
+    );
+  }
+};
+
+class RenderInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Input Render:</h3>
+        <p>{this.props.input}</p>
+      </div>
+    );
+  }
+};
+
+
+/*
+Use the Lifecycle Method componentWillMount
+React components have several special methods that provide opportunities to perform actions at specific points in the lifecycle of a component. 
+These are called lifecycle methods, or lifecycle hooks, and allow you to catch components at certain points in time. 
+This can be before they are rendered, before they update, before they receive props, before they unmount, and so on. 
+Here is a list of some of the main lifecycle methods: componentWillMount() componentDidMount() 
+shouldComponentUpdate() componentDidUpdate() componentWillUnmount() The next several lessons will cover some of the basic use cases for these lifecycle methods.
+
+Note: The componentWillMount Lifecycle method will be deprecated in a future version of 16.X and removed in version 17. 
+Learn more in this article
+https://www.freecodecamp.org/news/how-to-safely-use-reacts-life-cycles-with-fiber-s-async-rendering-fd4469ebbd8f/
+
+
+The componentWillMount() method is called before the render() method when a component is being mounted to the DOM. 
+Log something to the console within componentWillMount() - you may want to have your browser console open to see the output. */
+
+/*
+Use the Lifecycle Method componentDidMount
+Most web developers, at some point, need to call an API endpoint to retrieve data. 
+If you're working with React, it's important to know where to perform this action.
+
+The best practice with React is to place API calls or any calls to your server in the lifecycle method componentDidMount(). 
+This method is called after a component is mounted to the DOM. Any calls to setState() here will trigger a re-rendering of your component. 
+When you call an API in this method, and set your state with the data that the API returns, it will automatically trigger an update once you receive the data.
+
+There is a mock API call in componentDidMount(). It sets state after 2.5 seconds to simulate calling a server to retrieve data. 
+This example requests the current total active users for a site. In the render method, render the value of activeUsers in the h1 after the text Active Users:. 
+Watch what happens in the preview, and feel free to change the timeout to see the different effects.
+*/
+
+
+
+
+
 
 
 
