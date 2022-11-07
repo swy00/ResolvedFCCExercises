fc8cdb7a6e59ea41859cab6606908fd77d1afa1d
Swy00 <franciscoronga00@gmail.com>
2022-11-06 21:11:27 -0300

    new

--- Front End Development Libraries/4-React/index.js
+++ Front End Development Libraries/4-React/index.js
@@ -1771,17 +1771,183 @@ Finally, make sure the handleClick() method is correctly counting each turn so t
 This also serves to let the user know the component has actually updated in case they win or lose twice in a row.
 */
 
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    {/* Change code below this line */}
+    return <h1> {this.props.fiftyFifty?'You Win!':'You Lose!'} </h1>;
+    {/* Change code above this line */}
+  }
+}
 
+class GameOfChance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState(prevState => {
+      // Complete the return statement:
+      return {
+        counter: this.state.counter+1
+      }
+    });
+  }
+  render() {
+    const expression = Math.random() >= .5?true:false; // Change this line
+    return (
+      <div>
+        <button onClick={this.handleClick}>Play Again</button>
+        {/* Change code below this line */}
+        <Results fiftyFifty={expression}/>
+        {/* Change code above this line */}
+        <p>{'Turn: ' + this.state.counter}</p>
+      </div>
+    );
+  }
+}
 
+/*
+Change Inline CSS Conditionally Based on Component State
+At this point, you've seen several applications of conditional rendering and the use of inline styles. 
+Here's one more example that combines both of these topics. You can also render CSS conditionally based on the state of a React component. 
+To do this, you check for a condition, and if that condition is met, you modify the styles object that's assigned to the JSX elements in the render method.
 
+This paradigm is important to understand because it is a dramatic shift from the more traditional approach of applying styles by modifying DOM elements directly 
+(which is very common with jQuery, for example). In that approach, you must keep track of when elements change and also handle the actual manipulation directly. 
+It can become difficult to keep track of changes, potentially making your UI unpredictable. 
+When you set a style object based on a condition, you describe how the UI should look as a function of the application's state. 
+There is a clear flow of information that only moves in one direction. This is the preferred method when writing applications with React.
 
+The code editor has a simple controlled input component with a styled border. You want to style this border red if the user types more than 15 characters of text in the input box.
+ Add a condition to check for this and, if the condition is valid, set the input border style to 3px solid red. You can try it out by entering text in the input.
+*/
 
+class GateKeeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ input: event.target.value })
+  }
+  render() {
+    let inputStyle = {
+      border: '1px solid black'
+    };
+    // Change code below this line
+    if(this.state.input.length>15){
+      inputStyle = {
+      border: '3px solid red'
+    };
+    }
+    // Change code above this line
+    return (
+      <div>
+        <h3>Don't Type Too Much:</h3>
+        <input
+          type="text"
+          style={inputStyle}
+          value={this.state.input}
+          onChange={this.handleChange} />
+      </div>
+    );
+  }
+};
 
+/*
+Use Array.map() to Dynamically Render Elements
+Conditional rendering is useful, but you may need your components to render an unknown number of elements. 
+Often in reactive programming, a programmer has no way to know what the state of an application is until runtime, because so much depends on a user's interaction with that program. 
+Programmers need to write their code to correctly handle that unknown state ahead of time. Using Array.map() in React illustrates this concept.
 
+For example, you create a simple "To Do List" app. As the programmer, you have no way of knowing how many items a user might have on their list. 
+You need to set up your component to dynamically render the correct number of list elements long before someone using the program decides that today is laundry day.
 
+The code editor has most of the MyToDoList component set up. Some of this code should look familiar if you completed the controlled form challenge. 
+You'll notice a textarea and a button, along with a couple of methods that track their states, but nothing is rendered to the page yet.
 
+Inside the constructor, create a this.state object and define two states: userInput should be initialized as an empty string, and toDoList should be initialized as an empty array. 
+Next, delete the null value in the render() method next to the items variable. 
+In its place, map over the toDoList array stored in the component's internal state and dynamically render a li for each item. 
+Try entering the string eat, code, sleep, repeat into the textarea, then click the button and see what happens.
 
+Note: You may know that all sibling child elements created by a mapping operation like this do need to be supplied with a unique key attribute. 
+Don't worry, this is the topic of the next challenge.
+*/
 
+const textAreaStyles = {
+  width: 235,
+  margin: 5
+};
+
+class MyToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    // Change code below this line
+    this.state={
+      userInput:'',
+      toDoList:[]
+    }
+    // Change code above this line
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit() {
+    const itemsArray = this.state.userInput.split(',');
+    this.setState({
+      toDoList: itemsArray
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value
+    });
+  }
+  render() {
+    const items = this.state.toDoList.map(i=><li>{i}</li>); // Change this line
+    return (
+      <div>
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.userInput}
+          style={textAreaStyles}
+          placeholder='Separate Items With Commas'
+        />
+        <br />
+        <button onClick={this.handleSubmit}>Create List</button>
+        <h1>My "To Do" List:</h1>
+        <ul>{items}</ul>
+      </div>
+    );
+  }
+}
+
+/*
+Give Sibling Elements a Unique Key Attribute
+The last challenge showed how the map method is used to dynamically render a number of elements based on user input. 
+However, there was an important piece missing from that example. 
+When you create an array of elements, each one needs a key attribute set to a unique value. 
+React uses these keys to keep track of which items are added, changed, or removed. This helps make the re-rendering process more efficient when the list is modified in any way.
+
+Note: Keys only need to be unique between sibling elements, they don't need to be globally unique in your application.
+
+The code editor has an array with some front end frameworks and a stateless functional component named Frameworks(). 
+Frameworks() needs to map the array to an unordered list, much like in the last challenge. 
+Finish writing the map callback to return an li element for each framework in the frontEndFrameworks array. 
+This time, make sure to give each li a key attribute, set to a unique value. The li elements should also contain text from frontEndFrameworks.
+
+Normally, you want to make the key something that uniquely identifies the element being rendered. 
+As a last resort the array index may be used, but typically you should try to use a unique identification.
+*/
 
 
 
