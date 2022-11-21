9f749879dd50f9ab49424e57995fe1963ff14efc
Swy00 <franciscoronga00@gmail.com>
2022-11-21 02:39:51 -0300

    Finished IAS

--- JavaScript Algorithms and Data Structures/FCC-IAS.js
+++ JavaScript Algorithms and Data Structures/FCC-IAS.js
@@ -414,6 +414,127 @@ In JavaScript, truthy values are values that translate to true when evaluated in
 
 Remember, you can access object properties through either dot notation or [] notation. */
 
+function truthCheck(collection, pre) {
+  for(let i=0 ; i < collection.length ;i++){
+    if(!collection[i][pre])
+      return false;
+  }
+  return true;
+}
 
+truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot");
 
+/*
+Arguments Optional
+Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+
+For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+
+Calling this returned function with a single argument will then return the sum:
+
+var sumTwoAnd = addTogether(2);
+sumTwoAnd(3) returns 5.
+
+If either argument isn't a valid number, return undefined.
+*/
+
+function addTogether() {
+  
+  const a = arguments[0];
+  const b = arguments[1];
+  const isNum = function(arg) { return Number.isFinite(arg); };
+  
+  if (arguments.length === 1 && isNum(a)) {
+    return function(x) {
+      if (isNum(x)) {
+        return a + x;
+      }
+    };
+  }
+  else if (arguments.length === 2 && isNum(a) && isNum(b)) {
+    return a + b;
+  }
+}
+
+addTogether(2,3);
+
+/*
+Make a Person
+Fill in the object constructor with the following methods below:
+
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(firstAndLast)
+
+Run the tests to see the expected output for each method. 
+The methods that take an argument must accept only one argument and it has to be a string. 
+These methods must be the only available means of interacting with the object.
+*/
+
+const Person = function(firstAndLast) {
+  let fullName = firstAndLast;
+
+  this.getFirstName = function() {
+    return fullName.split(" ")[0];
+  };
+
+  this.getLastName = function() {
+    return fullName.split(" ")[1];
+  };
+
+  this.getFullName = function() {
+    return fullName;
+  };
+
+  this.setFirstName = function(name) {
+    fullName = name + " " + fullName.split(" ")[1];
+  };
+
+  this.setLastName = function(name) {
+    fullName = fullName.split(" ")[0] + " " + name;
+  };
+
+  this.setFullName = function(name) {
+    fullName = name;
+  };
+};
+
+const bob = new Person('Bob Ross');
+//console.log(bob.getFullName());
+bob.getFullName();
+
+/*
+Map the Debris
+According to Kepler's Third Law, the orbital period  T  of two point masses orbiting each other in a circular or elliptic orbit is:
+
+T=2πa3μ−−−√
+ 
+a  is the orbit's semi-major axis
+μ=GM  is the standard gravitational parameter
+G  is the gravitational constant,
+M  is the mass of the more massive body.
+Return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+
+The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+
+The values should be rounded to the nearest whole number. The body being orbited is Earth.
+
+The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+*/
+
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+
+  arr.forEach(function(item) {
+    item.orbitalPeriod = Math.round(2 * Math.PI * Math.sqrt(Math.pow(earthRadius + item.avgAlt, 3) / GM));;
+    delete item.avgAlt;
+  });
+  return arr;
+}
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
 
